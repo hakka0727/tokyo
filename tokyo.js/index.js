3bcbb7b8db1b25ba76c30b93e44a257e02916b46
haruka0727 <91817129+haruka0727@users.noreply.github.com>
2022-01-06 18:17:47 +0900

    Update index.js

--- tokyo.js/index.js
+++ tokyo.js/index.js
@@ -175,3 +175,16 @@ function stopload(){
 
 
 
+
+
+
+
+const popup = document.getElementById('popup');
+const close = document.getElementById('close');
+const popWin = document.getElementById('popWin');
+popup.addEventListener('click', () => {
+    popWin.style.display = 'flex';
+});
+close.addEventListener('click', () => {
+    popWin.style.display = 'none';
+});
